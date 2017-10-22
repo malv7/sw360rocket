9187a83a4e3cdc863f0fd9509e0f856fd2ba77b8
Tarek S <tarek.schneider@beuth-hochschule.de>
2017-10-22 21:21:43 +0200

    ITCSS Styling added + SemanticUI

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<posts-root></posts-root>`
+  template: `<posts-root></posts-root>`,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent { }
