ce2a2b3743fb0cfb78d5c2648e314c4c6432a5b3
malv <malvin@danhof.de>
2017-10-22 12:26:24 +0200

    status quo

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { MalvModule } from './malv/malv.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -11,8 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    MalvModule
   ],
   providers: [],
   bootstrap: [AppComponent]
