a1692ad1a532d8d1e284d625cdeda4f4c368fb12
Malvin Danhof <fixed-term.malvin.danhof@bosch-si.com>
2017-11-21 21:31:31 +0100

    random dummy component data

--- src/app/component/components/component/component.component.ts
+++ src/app/component/components/component/component.component.ts
@@ -1,4 +1,15 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+
+interface MichiDummyComponent {
+  name: string;
+  description: string;
+}
+
+const DUMMY_COMPONENT: MichiDummyComponent = {
+  name: 'xyz',
+  description: 'abc'
+}
 
 @Component({
   selector: 'sw-component',
@@ -10,6 +21,11 @@ export class ComponentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.getDummyData();
+  }
+
+  getDummyData(): Observable<MichiDummyComponent> {
+    return Observable.of(DUMMY_COMPONENT);
   }
 
 }
