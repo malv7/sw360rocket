28c827f9f9f5548d0a230beecb9dac0e017b767d
Michael Tebbe <michaeltebbe1991@gmail.com>
2017-12-07 23:55:36 +0100

    Subrouting for Tabs

--- src/app/shared/components/tab-navigation/tab-navigation.component.ts
+++ src/app/shared/components/tab-navigation/tab-navigation.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+export interface Tab{
+routerlink:string;
+title:string;
+classActive:string;
+classInactive:string;
+}
 @Component({
   selector: 'sw-tab-navigation',
   templateUrl: './tab-navigation.component.html',
   styleUrls: ['./tab-navigation.component.scss']
 })
 export class TabNavigationComponent implements OnInit {
-
+@Input() tabs: Tab[]
   constructor() { }
 
   ngOnInit() {
+		//this.tabs = [{routerlink: 'releases', title: 'Releases'}];
   }
 
 }
