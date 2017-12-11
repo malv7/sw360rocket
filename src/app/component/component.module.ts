129041f7c389387bdcd46c57289aadd3075c3cce
Michael Tebbe <michaeltebbe1991@gmail.com>
2017-12-11 17:55:44 +0100

    Pagination view without logic

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -7,7 +7,7 @@ import { SingleComponentComponent } from './components/single-component/single-c
 import { ReleasesComponent } from './components/single-component/releases/releases.component';
 import { ComponentRootComponent } from './components/component-root/component-root.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AttachmentsComponent } from './components/single-component/attachments/attachments/attachments.component';
+import { AttachmentsComponent } from './components/single-component/attachments/attachments.component';
 
 @NgModule({
   imports: [
