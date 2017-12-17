f792562724c3700cf724f01b187fd348f6538270
malv <malvin@danhof.de>
2017-12-17 13:39:16 +0100

    removed app.routing module since there is a new sw-routing.module in router feature. adjusted paths in sw routing module for components.  adjusted routing in component list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 
 // App root component
 import { AppComponent } from './app.component';
