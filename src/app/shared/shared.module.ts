0c22cef6d3a8507b44adbf79b865b7b74ae2be3f
malv <malvin@danhof.de>
2017-12-19 22:41:25 +0100

    renamed again

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -11,7 +11,7 @@ import { StoreModule } from '@ngrx/store';
 import { listSelectReducer } from './state/list-select/list-select.reducer';
 import { ActionButtonsComponent } from './components/action-buttons/action-buttons.component';
 import { SearchFilterComponent } from './components/search-filter/search-filter.component';
-import { ReleaseTableComponent } from './tables/release-table.component/release-table.component';
+import { ReleaseTableComponent } from './tables/release-table/release-table.component';
 import { FormsModule } from '@angular/forms';
 import { TableService } from './tables/services/table.service';
 
