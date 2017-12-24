287da5ad511466184fcb0765a343a4025b03c44c
Tarek Es <treksny@gmail.com>
2017-12-24 15:49:32 +0100

    description + observable added

--- src/app/component/components/single-component/detail/detail.component.ts
+++ src/app/component/components/single-component/detail/detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromRoot from './../../../../reducers';
+import { ComponentDataLayout, ResolvedRelease } from '../../../state/component.models';
+import { Router, Routes } from '@angular/router';
+import * as RouterActions from './../../../../router/state/router.actions';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'sw-detail',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  components: Observable<ComponentDataLayout[]>;
+	currentComponent: ComponentDataLayout;
+  constructor(private store: Store<fromRoot.State>, public router: Router) { }
+  
 
   ngOnInit() {
+    this.components = this.store.select(fromRoot.selectComponents);
+		this.components.subscribe(componentData => {
+      this.currentComponent=componentData[0];
+      console.log(this.currentComponent);
+    } )
   }
 
 }
