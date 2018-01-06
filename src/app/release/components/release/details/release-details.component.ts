563e7e61f8a7012d04cebca9b9e55bce0d3f1ad9
malv <malvin@danhof.de>
2018-01-06 13:15:30 +0100

    release-details mock data

--- src/app/release/components/release/details/release-details.component.ts
+++ src/app/release/components/release/details/release-details.component.ts
@@ -1,11 +1,26 @@
 import { Component } from "@angular/core";
+import * as fromModel from './../../../../state/model.reducer';
+import * as fromRoot from './../../../../reducers';
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/Observable";
+import { Release } from "../../../../state/models";
 
 @Component({
     selector: 'sw-release-details',
     template: `
-        release detail
+        {{ release | async | json }}
+
+        Release Mainline State: Open || Closed || LONG_LONG_LONG
+        
+        type: OSS || FREESOFTWARE || LONG_LONG_LONG
     `
 })
 export class ReleaseDetailsComponent {
+
+  release: Observable<Release>;
+
+  constructor(private store: Store<fromRoot.State>) {
+    this.release = store.select(fromModel.selectRelease);
+  }
 
 }
