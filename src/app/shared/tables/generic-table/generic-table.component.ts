28387d4c81ea942234f154f068b05bd75126b0dc
Tarek S <tarek.schneider@beuth-hochschule.de>
2018-01-10 21:58:35 +0100

    refactored generic table

--- src/app/shared/tables/generic-table/generic-table.component.ts
+++ src/app/shared/tables/generic-table/generic-table.component.ts
@@ -35,131 +35,27 @@ export class GenericTableComponent implements OnInit, OnDestroy {
 	@Input('tableHeader') tableHeader: TemplateRef<Component>;
 	@Input('tableBody') tableBody: TemplateRef<Component>;
 
-  // Ressources
-  components: Observable<EmbeddedSW360Component[]>
-  projects: Observable<EmbeddedProject[]>
-  releases: Observable<EmbeddedRelease[]>
-
   // Selection state
-  areAllSelected: boolean;
-  selectedElementsCount: number;
-  selectedElementsCountSub: Subscription;
+  selectedElementsCount: Observable<number>;
 
   // Table size affects styling
   tableSize: string = '';
-
-  // DEBUG
-  debug_tableIterator = 1;
-  debug_tabletypes = [TableTypes.projects, TableTypes.components, TableTypes.releases];
 
   @Input() type: string; // TableTypes
   @Input() debug: boolean = false;
 
   constructor(private store: Store<fromRoot.State>) {
-
-    this.selectedElementsCountSub = this.store.select(fromTable.selectSelectedListElementsCount)
-      .subscribe(selectedElementsCount => {
-        this.selectedElementsCount = selectedElementsCount;
-        if (selectedElementsCount === 0)
-          this.areAllSelected = false;
-      });
-
-    // Observable.interval(5000).subscribe(() => this.next());
-  }
-
-  // DEBUG
-  next() {
-    this.debug_tableIterator++;
-    if (this.debug_tableIterator > this.debug_tabletypes.length - 1) this.debug_tableIterator = 0;
-    this.type = this.debug_tabletypes[this.debug_tableIterator];
-    this.store.dispatch(new TableActions.ClearTableSelections());
-    this.areAllSelected = false;
+    this.selectedElementsCount = this.store.select(fromTable.selectSelectedListElementsCount);
   }
 
   ///// Lifecycle
   ///////////////
   ngOnInit(): void {
-    this.initializeTable();
   }
 
   ngOnDestroy(): void {
     this.store.dispatch(new TableActions.ClearTableSelections());
-    if (this.selectedElementsCountSub) this.selectedElementsCountSub.unsubscribe();
   }
-
-  ///// Initialize
-  ////////////////
-  // TODO: Suscribe to NewTableData Action here and reinit the table
-  // This happpens when pagination changes
-  initializeTable() {
-    if (this.debug) {
-      this.type = this.debug_tabletypes[this.debug_tableIterator];
-    }
-    // console.log(this.type);
-
-    // TODO: Get type from store here
-    // TODO: Wait for finished route
-    this.initializeTableResource();
-    this.initializeSelection();
-  }
-
-  initializeSelection() {
-    this.areAllSelected = false;
-    this.selectedElementsCount = 0;
-    this.store.dispatch(new TableActions.ClearTableSelections());
-  }
-
-  initializeTableResource() {
-
-    // DEBUG
-    if (this.debug) {
-      this.projects = this.store.select(fromModel.selectProjects);
-      this.components = this.store.select(fromModel.selectComponents);
-      this.releases = this.store.select(fromModel.selectReleases);
-    }
-
-    switch (this.type) {
-      case TableTypes.projects: {
-        this.projects = this.store.select(fromModel.selectProjects);
-        break;
-      }
-      case TableTypes.components: {
-        this.components = this.store.select(fromModel.selectComponents);
-        break;
-      }
-      case TableTypes.releases: {
-        this.releases = this.store.select(fromModel.selectReleases);
-        break;
-      }
-      default: {
-        break;
-      }
-    }
-  }
-
-  // ///// Routing
-  // go(resource: EmbeddedResource) {
-  //   this.store.dispatch(new RouterActions.Go({ path: [], ressourceSelflink: resource._links.self.href }));
-  // };
-
-  // ///// Selections
-  // ////////////////
-  // selectOne(resource: EmbeddedResource) {
-  //   this.store.dispatch(new TableActions.ToggleTableSelection(resource._links.self.href));
-  // }
-
-  // selectAll() {
-  //   if (this.areAllSelected) {
-  //     this.store.dispatch(new TableActions.ClearTableSelections());
-  //   } else {
-  //     const selfLinks: string[] = [];
-  //     const resources: Observable<EmbeddedRessourceX[]> = this.getTableResource();
-  //     resources.take(1).subscribe(resources => {
-  //       resources.forEach(resource => selfLinks.push(resource._links.self.href));
-  //       this.store.dispatch(new TableActions.AddTableSelections(selfLinks));
-  //     });
-  //   }
-  // }
 
   ///// Style
   ///////////
@@ -167,18 +63,6 @@ export class GenericTableComponent implements OnInit, OnDestroy {
     this.tableSize = size; // small, normal, large
     // TODO: Action
   }
-
-  ///// Helpers
-  /////////////
-  getTableResource() {
-    switch (this.type) {
-      case TableTypes.projects: return this.projects;
-      case TableTypes.components: return this.components;
-      case TableTypes.releases: return this.releases;
-      default: return undefined; // never happens
-    }
-  }
-
 }
 
 // TODO:
