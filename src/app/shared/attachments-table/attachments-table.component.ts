5d24f9cad4b82c65e1e8fd8fb076f8d4abcbb80c
Michael Tebbe <michaeltebbe1991@gmail.com>
2018-01-19 16:49:56 +0100

    Component created

---
+++ src/app/shared/attachments-table/attachments-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sw-attachments-table',
+  templateUrl: './attachments-table.component.html',
+  styleUrls: ['./attachments-table.component.scss']
+})
+export class AttachmentsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
